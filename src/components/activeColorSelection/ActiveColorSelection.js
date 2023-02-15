4f25b1d15e357f8462d6ce555ae294d8fe2e8483
Igor <iluzarraga@gmail.com>
2023-02-15 16:04:55 +0100

    new funcionality: add and remove color selection (Pallette)

--- src/components/activeColorSelection/ActiveColorSelection.js
+++ src/components/activeColorSelection/ActiveColorSelection.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { faCircle } from "@fortawesome/free-regular-svg-icons";
+// import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import { faSquarePlus } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { setActiveCircle } from '../../features/colorSelector/colorSelectorSlice'
 
 import './style.css'
 
-
-const ActiveColorSelection = () => {
+const ActiveColorSelection = ({pickerColor}) => {
     const list = useSelector((state) => state.colorSelector.activeColorSelection)
     
     return (
@@ -15,35 +15,49 @@ const ActiveColorSelection = () => {
             <ul 
                 className='ulStyle'
             >
-                {renderListItems(list)}
+                {renderListItems(list, pickerColor)}
             </ul>
         </div>
     );
 }
 
-const changeIconStyle = (currentCircleId, activeCircleId) => 
-    currentCircleId === activeCircleId ? 'iconActive' : 'iconDesactive'
+// const changeIconStyle = (currentCircleId, activeCircleId) => 
+//     currentCircleId === activeCircleId ? 'iconActive' : 'iconDesactive'
 
-    
+const changeIconStyle = (currentCircleId, activeCircleId, color, pickerColor) => {
+    const {r, g, b} = pickerColor
 
-const ListItem = ({circle}) => {
+    const iconActive = {
+        'font-size': '35px',
+        background: `rgb(${r}, ${g}, ${b})` 
+    }
+
+    const iconDesactive = {
+        'font-size': '25px',
+        background: color
+    }
+
+    return currentCircleId === activeCircleId ? iconActive : iconDesactive
+}
+
+const ListItem = ({circle, pickerColor}) => {
     const {color, id} = circle
     const dispatch = useDispatch()
     const activeCircleId = useSelector((state) => state.colorSelector.activeCircle)
-    
     return(
         <li>
             <button
                 className='btnStyle'
                 onClick={ () => dispatch(setActiveCircle(id))}
             > 
-              <FontAwesomeIcon icon={faCircle} className={changeIconStyle(id, activeCircleId)} />
+              {/* <FontAwesomeIcon icon={faCircle} className={changeIconStyle(id, activeCircleId)} /> */}
+              <FontAwesomeIcon icon={faSquarePlus} style={changeIconStyle(id, activeCircleId, color, pickerColor)} />
             </button>
         </li>
     )   
 }
 
-const renderListItems = (list) =>
-    list.map((item, index) => <ListItem key={index} circle={item} />)
+const renderListItems = (list, pickerColor) =>
+    list.map((item, index) => <ListItem key={index} circle={item} pickerColor={pickerColor}/>)
 
 export default ActiveColorSelection
