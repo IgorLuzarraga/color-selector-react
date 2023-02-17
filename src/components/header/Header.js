0bb095a42be3c22d438a6f3bf30f27ed28a0eba9
Igor <iluzarraga@gmail.com>
2023-02-17 11:23:54 +0100

    remove old commented code

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,6 @@ import { faPalette } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import './header.css'
 
-// const componentStyle = {
-//     display: 'flex',
-//     'flex-direction': 'row',
-//     margin: '5px',
-//     gap: '10px'
-// }
-
 const Header = () => {
     return(
         <header className='header'>
