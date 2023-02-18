dc2e1316665ce598f33eb9b47a3ba1877dcce426
Igor <iluzarraga@gmail.com>
2023-02-18 08:32:56 +0100

    removed counter feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import colorSelectorReducer from '../features/colorSelector/colorSelectorSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     colorSelector: colorSelectorReducer
   },
 });
